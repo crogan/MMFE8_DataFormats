bb84abf1cde9accf388b2b04d9705b307f2cee59
sezata <sezata@gmail.com>
2017-05-16 13:54:24 -0400

    changed combiner to not load everything into memory

--- python/combineTPGBT.py
+++ python/combineTPGBT.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # combines GBT and tp root files to one
-# cannabilized code from Alex
+# cannibalized code from Alex
 
 import ROOT, sys
 import time as tm
@@ -34,58 +34,12 @@ def main(argv):
     # get all gbt information
     tstart = tm.time()
     ngbt = gbt_tree.GetEntries()
-    while gbt_tree.GetEntry(i):
-        i += 1
-        time = gbt_tree.Time_sec + gbt_tree.Time_nsec/pow(10.,9)
-        #if (i % 100000 == 0):
-        #    print "Unpacking GBT Event",i
-
-        gbtpk = GBTPacket(time)
-
-        mmfes = [im for im in gbt_tree.gbt_MMFE8]
-        vmms = [iv for iv in gbt_tree.gbt_VMM]
-        chs = [ich for ich in gbt_tree.gbt_CH]
-        bcids = [ib for ib in gbt_tree.gbt_BCID]
-
-        for j in range(len(mmfes)):
-            gbtpk.add(mmfes[j],vmms[j],chs[j],bcids[j])
-
-        gbtpk.pad()
-        gbtpks.append(gbtpk)
-        progress(tm.time()-tstart, i, ngbt)
-
-    print "\nLoaded GBT Events"
-    i = 0
     # get all fit information
     tstart = tm.time()
     nfit = tpfit_tree.GetEntries()
-    while tpfit_tree.GetEntry(i):
-        i += 1
-#        if (i % 100000 == 0):
-#            print "Unpacking FIT event",i
-        time = tpfit_tree.Time_sec + tpfit_tree.Time_nsec/pow(10.,9)
-        bcid = tpfit_tree.BCID
-        mxloc = tpfit_tree.mxlocal
-        nevent = tpfit_tree.EventNum
-        nhit = tpfit_tree.tpfit_n
-        cntr = tpfit_tree.cntr
 
-        fitpk = FitPacket(nevent,time,bcid,mxloc,nhit,cntr)
-        
-        mmfes = [im for im in tpfit_tree.tpfit_MMFE8]
-        vmms = [iv for iv in tpfit_tree.tpfit_VMM]
-        chs = [ich for ich in tpfit_tree.tpfit_CH]
-
-        for j in range(len(mmfes)):
-            fitpk.add(mmfes[j],vmms[j],chs[j])
-        fitpks.append(fitpk)
-        progress(tm.time()-tstart, i, nfit)
-
-    print "\nLoaded FIT events"
-    ngbtpks = len(gbtpks)
-    nfitpks = len(fitpks)
-    print "Nfit events", nfitpks
-    print "Ngbt events", ngbtpks
+    print "Nfit events", nfit
+    print "Ngbt events", ngbt
     currifitpk = 0
 
     output = ROOT.TFile(args.o,"recreate")
@@ -117,63 +71,113 @@ def main(argv):
 
     tstart = tm.time()
     matchedFitEvents = []
-    for ig, gbtpk in enumerate(gbtpks):
+    i = 0
+    while gbt_tree.GetEntry(i):
+        i += 1
+        time = gbt_tree.Time_sec + gbt_tree.Time_nsec/pow(10.,9)
+
+        gbtpk = GBTPacket(time)
+
+        mmfes = [im for im in gbt_tree.gbt_MMFE8]
+        vmms = [iv for iv in gbt_tree.gbt_VMM]
+        chs = [ich for ich in gbt_tree.gbt_CH]
+        bcids = [ib for ib in gbt_tree.gbt_BCID]
+
+        for j in range(len(mmfes)):
+            gbtpk.add(mmfes[j],vmms[j],chs[j],bcids[j])
+            
+        gbtpk.pad()
         j = currifitpk
         while True:
-            if (j == nfitpks):
+            tpfit_tree.GetEntry(j)
+
+            if (j > nfit):
                 break
-            if (fitpks[j].time - gbtpk.time) > 0.1: #we've gone too far
+
+            time = tpfit_tree.Time_sec + tpfit_tree.Time_nsec/pow(10.,9)
+            bcid = tpfit_tree.BCID
+            mxloc = tpfit_tree.mxlocal
+            nevent = tpfit_tree.EventNum
+            nhit = tpfit_tree.tpfit_n
+            cntr = tpfit_tree.cntr
+
+            fitpk = FitPacket(nevent,time,bcid,mxloc,nhit,cntr)
+
+            mmfes = [im for im in tpfit_tree.tpfit_MMFE8]
+            vmms = [iv for iv in tpfit_tree.tpfit_VMM]
+            chs = [ich for ich in tpfit_tree.tpfit_CH]
+
+            for k in range(len(mmfes)):
+                fitpk.add(mmfes[k],vmms[k],chs[k])
+            if (fitpk.time - gbtpk.time) > 0.1: #we've gone too far
                 break
-            if abs(fitpks[j].time - gbtpk.time) < 0.1:
-                if fitpks[j].bcid in gbtpk.bcids:
+            if abs(fitpk.time - gbtpk.time) < 0.1:
+                if fitpk.bcid in gbtpk.bcids:
                     matchedFitEvents.append(j)
                     for (mmfe, vmm, ch, bcid) in gbtpk.hits:
-                        for (tpmmfe, tpvmm, tpch) in fitpks[j].hits:
+                        for (tpmmfe, tpvmm, tpch) in fitpk.hits:
                             if (mmfe, vmm, ch) == (tpmmfe,tpvmm,tpch):
-                                fitpks[j].addmod(tpmmfe, tpvmm, tpch, bcid)
+                                fitpk.addmod(tpmmfe, tpvmm, tpch, bcid)
                                 t_tpfit_VMM.push_back(tpvmm)
                                 t_tpfit_CH.push_back(tpch)
                                 t_tpfit_MMFE8.push_back(tpmmfe)
                                 t_tpfit_BCID.push_back(bcid)
                     currifitpk = j+1
-                    t_mxlocal[0] = fitpks[j].mxl
-                    t_bcid[0] = fitpks[j].bcid
-                    t_tpfit_n[0] = fitpks[j].nhit
-                    t_cntr[0] = fitpks[j].cntr
-                    t_eventnum[0] = fitpks[j].nevent
-                    t_timesec[0] = fitpks[j].timesec
-                    t_timensec[0] = fitpks[j].timensec
+                    t_mxlocal[0] = fitpk.mxl
+                    t_bcid[0] = fitpk.bcid
+                    t_tpfit_n[0] = fitpk.nhit
+                    t_cntr[0] = fitpk.cntr
+                    t_eventnum[0] = fitpk.nevent
+                    t_timesec[0] = fitpk.timesec
+                    t_timensec[0] = fitpk.timensec
                     t.Fill()
                     t_tpfit_VMM.clear()
                     t_tpfit_CH.clear()
                     t_tpfit_MMFE8.clear()
                     t_tpfit_BCID.clear()
             j += 1
-        if (j == nfitpks):
+        if (j == nfit+1):
             continue
-        progress(tm.time()-tstart, ig, ngbtpks)
+        progress(tm.time()-tstart, i, ngbt)
     print "\nChecking for missed fit packets"
     nmiss = 0
     tstart = tm.time()
     setmatched = set(matchedFitEvents)
-    setall = set(range(nfitpks))
+    setall = set(range(1,nfit))
     missing = setall.difference(setmatched)
     #print "Missing events: ", missing
     # loop through missing events to add them with BCID -1
     for k in missing:
         nmiss += 1
-        for (tpmmfe, tpvmm, tpch) in fitpks[k].hits:
+        tpfit_tree.GetEntry(k)
+        time = tpfit_tree.Time_sec + tpfit_tree.Time_nsec/pow(10.,9)
+        bcid = tpfit_tree.BCID
+        mxloc = tpfit_tree.mxlocal
+        nevent = tpfit_tree.EventNum
+        nhit = tpfit_tree.tpfit_n
+        cntr = tpfit_tree.cntr
+        
+        fitpk = FitPacket(nevent,time,bcid,mxloc,nhit,cntr)
+        
+        mmfes = [im for im in tpfit_tree.tpfit_MMFE8]
+        vmms = [iv for iv in tpfit_tree.tpfit_VMM]
+        chs = [ich for ich in tpfit_tree.tpfit_CH]
+
+        for l in range(len(mmfes)):
+            fitpk.add(mmfes[l],vmms[l],chs[l])
+            
+        for (tpmmfe, tpvmm, tpch) in fitpk.hits:
             t_tpfit_VMM.push_back(tpvmm)
             t_tpfit_CH.push_back(tpch)
             t_tpfit_MMFE8.push_back(tpmmfe)
             t_tpfit_BCID.push_back(-1)
-        t_mxlocal[0] = fitpks[k].mxl
-        t_bcid[0] = fitpks[k].bcid
-        t_tpfit_n[0] = fitpks[k].nhit
-        t_cntr[0] = fitpks[k].cntr
-        t_eventnum[0] = fitpks[k].nevent
-        t_timesec[0] = fitpks[k].timesec
-        t_timensec[0] = fitpks[k].timensec
+        t_mxlocal[0] = fitpk.mxl
+        t_bcid[0] = fitpk.bcid
+        t_tpfit_n[0] = fitpk.nhit
+        t_cntr[0] = fitpk.cntr
+        t_eventnum[0] = fitpk.nevent
+        t_timesec[0] = fitpk.timesec
+        t_timensec[0] = fitpk.timensec
         t.Fill()
         t_tpfit_VMM.clear()
         t_tpfit_CH.clear()
